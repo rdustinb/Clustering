b65a3dba8c6246ab97e41e856b180d21fe103cac
Dustin Brothers <gentian.limited-0t@icloud.com>
2023-12-22 09:56:22 -0700

    Major update to the local data collector. Intended to be run from a cronjob every 1 minute, it collects 4 data samples on a 15 second interval and appends to a JSON archive. The script now limits the data to 1 hour of samples, for a total of 240 data points.

--- utilities/local_data_collector.py
+++ utilities/local_data_collector.py
@@ -5,15 +5,49 @@ try:
 except:
     print("There are libraries missing, please install psutil, time, and json.")
 
-SampleSeconds = 55
+data_file = "data.json"
+max_data_samples = 240 # Store 1 hour of samples
 
-data_dict = {"cpu_count": psutil.cpu_count(), "mem_total": psutil.virtual_memory().total, "cpu_samples": list(), "mem_samples": list()}
+# Read in the JSON Data, or create a new dictionary to store...
+try:
+    with open(data_file, "r") as json_data:
+         data = json.load(json_data)
+except FileNotFoundError:
+    # Create an empty data dictionary
+    data = {"cpu_count": psutil.cpu_count(), "mem_total": psutil.virtual_memory().total, "cpu_samples": list(), "mem_samples": list()}
 
-# Fetch the Samples...
-for thisSample in range(SampleSeconds):
-    data_dict["cpu_samples"].append(psutil.cpu_percent())
-    data_dict["mem_samples"].append(psutil.virtual_memory().available)
-    time.sleep(1)
+time.sleep(1)
 
-with open("data.json", 'w+', encoding="utf-8") as fh:
-    json.dump(data_dict, fh, ensure_ascii=False, indent=2)
+# This kicks off every minute, and there are samples take in this script every 15 seconds
+for thisSample in range(4):
+    # Fetch and append a Sample...
+    data["cpu_samples"].append(psutil.cpu_percent())
+    data["mem_samples"].append(psutil.virtual_memory().available)
+    # Wait 15s, but not on the last sample
+    if thisSample != 3: time.sleep(15)
+
+# Get length of data arrays
+cpu_sample_length = len(data["cpu_samples"])
+mem_sample_length = len(data["mem_samples"])
+
+# Trim the CPU data if necessary...
+if cpu_sample_length > max_data_samples:
+    print("Length of CPU data is %d, trimming..."%(cpu_sample_length))
+    thisTrim = cpu_sample_length - max_data_samples
+    # Remove the oldest samples...
+    del data["cpu_samples"][:thisTrim]
+    cpu_sample_length = len(data["cpu_samples"])
+    print("Length of CPU data is now %d."%(cpu_sample_length))
+
+# Trim the Memory data if necessary...
+if mem_sample_length > max_data_samples:
+    print("Length of Memory data is %d, trimming..."%(mem_sample_length))
+    thisTrim = mem_sample_length - max_data_samples
+    # Remove the oldest samples...
+    del data["mem_samples"][:thisTrim]
+    mem_sample_length = len(data["mem_samples"])
+    print("Length of Memory data is now %d."%(mem_sample_length))
+
+# Store the data
+with open(data_file, 'w+', encoding="utf-8") as fh:
+    json.dump(data, fh, ensure_ascii=False, indent=2)
